f8785c9ac4f653843ec9d25268b0812e22bf92a8
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-03 23:12:14 +0800

    add profile __str__

--- backend/models/profile.py
+++ backend/models/profile.py
@@ -6,3 +6,6 @@ from .base import BaseModel
 class Profile(BaseModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
     company = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return str(self.user)

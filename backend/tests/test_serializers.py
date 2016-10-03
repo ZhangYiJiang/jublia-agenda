777983f01b3067df8f3d515574929b5f9eeadf72
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-04 01:06:03 +0800

    extract base serializer testcase class methods into functions for easier reuse

--- backend/tests/test_serializers.py
+++ backend/tests/test_serializers.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from django.test import TestCase
 from rest_framework.exceptions import ValidationError
 
@@ -6,25 +7,30 @@ from backend.models import *
 from backend.tests import data
 
 
+def create_user(data):
+    s = UserSerializer(data=data)
+    s.is_valid(raise_exception=True)
+    return s.save()
+
+
+@lru_cache(maxsize=None)
+def create_default_user():
+    return create_user(data.user)
+
+
+def create_agenda(data):
+    user = create_default_user()
+    s = AgendaSerializer(data=data, context={'user': user})
+    s.is_valid(True)
+    return s.save()
+
+
+def create_default_agenda():
+    return create_agenda(data.agenda)
+
+
 class SerializerTestCase(TestCase):
-    def _create_default_user(self):
-        if not hasattr(self, 'user'):
-            self.user = self._create_user(data.user)
-        return self.user
-
-    def _create_default_agenda(self):
-        return self._create_agenda(data.agenda)
-
-    def _create_user(self, data):
-        s = UserSerializer(data=data)
-        s.is_valid(raise_exception=True)
-        return s.save()
-
-    def _create_agenda(self, data):
-        user = self._create_default_user()
-        s = AgendaSerializer(data=data, context={'user': user})
-        s.is_valid(True)
-        return s.save()
+    pass
 
 
 class UserSerializerTest(SerializerTestCase):
@@ -34,30 +40,30 @@ class UserSerializerTest(SerializerTestCase):
         return s.save()
 
     def test_create_user(self):
-        user = self._create_user(data.user)
+        user = create_user(data.user)
         self.assertIsInstance(user.profile, Profile, 'Profile object not created with user')
 
     def test_create_user_with_profile(self):
-        self._create_user({
+        create_user({
             **data.user,
             'company': 'Hello World Corp.'
         })
 
     def test_invalid_user(self):
         with self.assertRaises(ValidationError, msg='Invalid email not rejected'):
-            self._create_user({
+            create_user({
                 'email': 'notemail',
                 'password': 'password123',
             })
 
         with self.assertRaises(ValidationError, msg='Password may not be blank'):
-            self._create_user({
+            create_user({
                 'email': 'exmaple@example.com',
                 'password': '',
             })
 
     def test_update_user(self):
-        u = self._create_user(data.user)
+        u = create_user(data.user)
         u = self._patch_user(u, {'company': 'Test Company'})
         self.assertEqual(u.profile.company, 'Test Company')
 
@@ -68,7 +74,7 @@ class UserSerializerTest(SerializerTestCase):
         self.assertEqual(u.profile.company, '')
 
     def test_invalid_update(self):
-        u = self._create_user(data.user)
+        u = create_user(data.user)
 
         with self.assertRaises(ValidationError):
             self._patch_user(u, {'email': 'invalid-email'})
@@ -81,10 +87,11 @@ class AgendaSerializerTest(SerializerTestCase):
         return s.save()
 
     def test_create_agenda(self):
-        self._create_agenda(data.agenda)
+        create_agenda(data.agenda)
+        create_agenda(data.full_agenda)
 
     def test_update_agenda(self):
-        agenda = self._create_agenda(data.agenda)
+        agenda = create_agenda(data.agenda)
 
         agenda = self._patch_agenda(agenda, {
             'name': 'Changed Event Name',
@@ -105,7 +112,7 @@ class AgendaSerializerTest(SerializerTestCase):
 
 class SessionSerializerTest(SerializerTestCase):
     def _create_session(self, data):
-        agenda = self._create_default_agenda()
+        agenda = create_default_agenda()
         s = SessionSerializer(data=data, context={'agenda': agenda})
         s.is_valid(True)
         return s.save()

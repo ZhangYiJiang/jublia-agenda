f5893790e896ba607eaa39b4411286d6e20dd733
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-07 16:15:19 +0800

    break up serializer tests

---
+++ backend/tests/serializers/test_session.py
@@ -0,0 +1,83 @@
+from rest_framework.exceptions import ValidationError
+
+from backend.serializers import SessionSerializer
+from backend.tests import factory
+from backend.tests.helper import create_user, create_agenda, create_session
+from backend.tests.serializers.test_serializers import SerializerTestCase
+
+
+class SessionSerializerTest(SerializerTestCase):
+    # Invalid test cases
+    negative_duration = {
+        'duration': -30,
+    }
+
+    start_without_duration = {
+        'start_at': factory.now.isoformat()
+    }
+
+    def setUp(self):
+        self.user = create_user(factory.user())
+        self.agenda = create_agenda(self.user, factory.agenda())
+
+    def update_session(self, session, data):
+        s = SessionSerializer(data=data, instance=session, partial=True)
+        s.is_valid(True)
+        return s.save()
+
+    def replace_session(self, session, data):
+        s = SessionSerializer(data=data, instance=session)
+        s.is_valid(True)
+        return s.save()
+
+    def test_create_session(self):
+        data = factory.session()
+        s = create_session(self.agenda, data)
+        self.assertEqual(s.name, data['name'])
+
+        data = factory.session(full=True)
+        s = create_session(self.agenda, data)
+        self.assertEqual(s.name, data['name'])
+        self.assertEqual(s.start_at, data['start_at'])
+        self.assertEqual(s.duration, data['duration'])
+        self.assertEqual(s.description, data['description'])
+
+    def test_update_session(self):
+        s = create_session(self.agenda, factory.session())
+
+        self.update_session(s, {
+            'duration': 45,
+            'start_at': 600,
+        })
+        self.assertEqual(45, s.duration)
+        self.assertEqual(600, s.start_at)
+
+        self.update_session(s, {
+            'name': 'The most amazing session ever',
+        })
+        self.assertEqual('The most amazing session ever', s.name)
+
+    def test_put_session(self):
+        s = create_session(self.agenda, factory.session(full=True))
+        data = factory.session()
+        self.replace_session(s, data)
+        self.assertEqual(s.name, data['name'])
+        self.assertIsNone(s.start_at)
+        self.assertIsNone(s.duration)
+        self.assertEqual(s.description, '')
+
+    def test_invalid_session(self):
+        with self.assertRaises(ValidationError):
+            create_session(self.agenda, self.negative_duration)
+
+        with self.assertRaises(ValidationError):
+            create_session(self.agenda, self.start_without_duration)
+
+    def test_invalid_update(self):
+        s = create_session(self.agenda, factory.session())
+
+        with self.assertRaises(ValidationError):
+            self.update_session(s, self.negative_duration)
+
+        with self.assertRaises(ValidationError):
+            self.update_session(s, self.start_without_duration)
---
+++ backend/tests/serializers/test_user.py
@@ -0,0 +1,51 @@
+from rest_framework.exceptions import ValidationError
+
+from backend.models import Profile
+from backend.serializers import UserSerializer
+from backend.tests import factory
+from backend.tests.helper import create_user
+from backend.tests.serializers.test_serializers import SerializerTestCase
+
+
+class UserSerializerTest(SerializerTestCase):
+    def _patch_user(self, user, data):
+        s = UserSerializer(user, data, partial=True)
+        s.is_valid(raise_exception=True)
+        return s.save()
+
+    def test_create_user(self):
+        user = create_user(factory.user())
+        self.assertIsInstance(user.profile, Profile, 'Profile object not created with user')
+
+    def test_create_user_with_profile(self):
+        create_user(factory.user(full=True))
+
+    def test_invalid_user(self):
+        with self.assertRaises(ValidationError, msg='Invalid email not rejected'):
+            create_user({
+                'email': 'notemail',
+                'password': 'password123',
+            })
+
+        with self.assertRaises(ValidationError, msg='Password may not be blank'):
+            create_user({
+                'email': 'exmaple@example.com',
+                'password': '',
+            })
+
+    def test_update_user(self):
+        u = create_user(factory.user())
+        u = self._patch_user(u, {'company': 'Test Company'})
+        self.assertEqual(u.profile.company, 'Test Company')
+
+        u = self._patch_user(u, {'company': 'Changed Company'})
+        self.assertEqual(u.profile.company, 'Changed Company')
+
+        u = self._patch_user(u, {'company': ''})
+        self.assertEqual(u.profile.company, '')
+
+    def test_invalid_update(self):
+        u = create_user(factory.user())
+
+        with self.assertRaises(ValidationError):
+            self._patch_user(u, {'email': 'invalid-email'})

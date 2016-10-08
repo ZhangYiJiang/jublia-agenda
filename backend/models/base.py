0aa498caf0509f87e3c249e70efc37990b718367
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-08 17:09:20 +0800

    make models iterable

--- backend/models/base.py
+++ backend/models/base.py
@@ -5,5 +5,10 @@ class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __iter__(self):
+        for field in self._meta.get_fields():
+            value = getattr(self, field.name, None)
+            yield (field.name, value)
+
     class Meta:
         abstract = True

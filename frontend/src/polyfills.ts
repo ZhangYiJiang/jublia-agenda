5b7ab84721edc902317fcd07ad288383b6549d89
Zhu Liang <zhuliangg11@gmail.com>
2016-10-10 21:06:45 +0800

    add intl polyfill

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -1,6 +1,8 @@
 import 'core-js/es6';
 import 'core-js/es7/reflect';
 import 'web-animations-js';
+import 'intl/index';
+import 'intl/locale-data/jsonp/en.js';
 require('zone.js/dist/zone');
 // require('web-animations-js/web-animations.min.js');
 if (process.env.ENV === 'production') {

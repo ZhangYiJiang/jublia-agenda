50a7302d945e21474eeb591986fe4e1913f36e6a
twink1e <tkgs.si.junke@gmail.com>
2016-10-14 22:57:52 +0800

    change data class for api

--- frontend/src/app/session/session.ts
+++ frontend/src/app/session/session.ts
@@ -1,13 +1,16 @@
+import { Speaker } from '../speaker/speaker';
+
 export class Session {
-  id: string;
-  title: string;
-  abstract: string;
-  track: string;
-  order: number;
-  pending: boolean;
-  start: Date;
-  end: Date;
-  duration: number;
+  id: number;
+  name: string;
+  desciption: string;
+  track: {id: number, name: string, url: string};
+  speakers: Speaker[];
+  start_at: number;  //number of minutes since start of the event
+  duration: number;  //number of minutes
+  url: string;
+  //not from api
   venue: string;
   placeholder: boolean;
+  order: number;
 }
---
+++ frontend/src/app/speaker/speaker.ts
@@ -0,0 +1,11 @@
+export class Speaker {
+	company: string;
+	company_description: string;
+	company_url: string;
+	email: string;
+	id: number;
+	name: string;
+	phone_number: string;
+	position: string;
+	url: string;
+}

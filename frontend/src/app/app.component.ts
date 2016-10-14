ec6c67e8d2189637cc50b3314368ec72ccc0fa0d
twink1e <tkgs.si.junke@gmail.com>
2016-10-15 00:52:21 +0800

    add nav for log out

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import '../../public/css/styles.css';
 import { DashBoardService } from './dash-board/dash-board.service';
+import { Router } from '@angular/router';
 
 import { GlobalVariable } from './globals';
 
@@ -13,10 +14,12 @@ export class AppComponent {
   appName: string = GlobalVariable.APP_NAME;
   user = this.dashBoardService.user;
 
-  constructor (private dashBoardService: DashBoardService) {}
+  constructor ( private router: Router,
+  	private dashBoardService: DashBoardService) {}
 
   logOut() {
   	console.log('log out');
     this.dashBoardService.logOut();
+    this.router.navigate(['']);
   }
 }

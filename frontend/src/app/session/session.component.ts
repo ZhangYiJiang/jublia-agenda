da6cfc61c303638891cb1d0dcd91fb9b2ef7ffe1
Zhu Liang <zhuliangg11@gmail.com>
2016-10-18 02:48:20 +0800

    removed unused themes

--- frontend/src/app/session/session.component.ts
+++ frontend/src/app/session/session.component.ts
@@ -24,13 +24,7 @@ import {
   styleUrls: [
   './session.component.css',
   './css/vex.css',
-  './css/vex-theme-default.css',
-  './css/vex-theme-os.css',
-  './css/vex-theme-plain.css',
-  './css/vex-theme-wireframe.css',
-  './css/vex-theme-flat-attack.css',
-  './css/vex-theme-top.css',
-  './css/vex-theme-bottom-right-corner.css'
+  './css/vex-theme-default.css'
   ],
   encapsulation: ViewEncapsulation.None
 })

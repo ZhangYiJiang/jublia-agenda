d891e953e7f3ccdfc40fcdd57d0d6c3f1f358c04
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-19 19:50:27 +0800

    make popularity uneditable

--- backend/models/session.py
+++ backend/models/session.py
@@ -27,7 +27,7 @@ class Session(BaseModel):
     ])
 
     # Denormalized aggregate of the number of registrations on the model
-    popularity = models.IntegerField(default=0)
+    popularity = models.IntegerField(default=0, editable=False)
 
     agenda = models.ForeignKey(Agenda, models.CASCADE)
     tags = models.ManyToManyField(Tag)

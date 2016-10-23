fdd402b1c9c19162d7f762b47a2be1c2d1009ac1
twink1e <tkgs.si.junke@gmail.com>
2016-10-23 16:48:54 +0800

    add default sign in detail

--- frontend/src/app/dash-board/dash-board.component.ts
+++ frontend/src/app/dash-board/dash-board.component.ts
@@ -24,8 +24,11 @@ export class DashBoardComponent implements OnInit {
   user = this.dashBoardService.currentUser;
   errorMsg: string;
   successMsg: string;
-  loginEmail: string;
-  loginPassword: string;
+  //loginEmail: string;
+  //loginPassword: string;
+  //for testing
+  loginEmail = 'meganmckenzie@gmail.com';
+  loginPassword = '^Z2AwhuJ)T';
   registerEmail: string;
   registerPassword: string;
   organiser: string;

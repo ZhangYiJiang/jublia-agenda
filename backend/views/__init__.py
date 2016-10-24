6f5387814fda12628c32bd8a8f05866ce601e3c2
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-10-24 14:20:53 +0800

    add analytics endpoint

--- backend/views/__init__.py
+++ backend/views/__init__.py
@@ -1,5 +1,7 @@
 from .agenda import AgendaList, AgendaDetail
+from .analytics import analytics
 from .session import SessionList, SessionDetail
 from .session_meta import TrackList, TrackDetail, SpeakerList, SpeakerDetail, CategoryViewSet, TagViewSet
 from .user import sign_up, UserDetail, ObtainJSONWebToken, verify_email, resend_verification, password_reset_confirm
 from .viewer import create_viewer, ViewerSessionList, ViewerRegistrationView
+

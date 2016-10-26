78d47592138071be39731f86afdf3d81c4b8518c
twink1e <tkgs.si.junke@gmail.com>
2016-10-27 00:36:29 +0800

    fix abs-col ovrflow

--- frontend/src/app/board/board.component.ts
+++ frontend/src/app/board/board.component.ts
@@ -66,7 +66,7 @@ export class BoardComponent implements OnInit, OnDestroy, AfterViewInit {
 
   dragging: boolean = false;
 
-  hours = ['8:00','9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00'];
+  hours = ['8:00','9:00','10:00','11:00','12:00','13:00','14:00','15:00','16:00','17:00','18:00','19:00','20:00','21:00'];
 
   private PLACEHOLDER_DURATION: number = 15;
 

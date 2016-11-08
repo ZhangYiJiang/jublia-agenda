748bc71e20652d6c1c0ae1875dea2a7eb6ad4d34
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-11-08 20:29:53 +0800

    update tag styling and button class names

--- frontend/src/app/globals.ts
+++ frontend/src/app/globals.ts
@@ -1,11 +1,15 @@
 export const GlobalVariable = Object.freeze({
   DEFAULT_NEW_DURATION: 60,
   APP_NAME: 'Agenda Builder',
+  
   TOKEN_NAME: 'auth_token',
+  
   BASE_URL: 'https://agenda-demo.jublia.com',
   API_BASE_URL: '/api/',
   PUBLIC_BASE_URL: '/public/',
+  
   GOOGLE_MAP_API_KEY: 'AIzaSyCqVB-KbI2IzISlF-ROOuI9AeZyBtx19f4',
+  
   COLOR_PRIMARY: {
     r: 65, 
     g: 182, 
@@ -14,6 +18,9 @@ export const GlobalVariable = Object.freeze({
     s: 47.4,
     l: 48.4,
   },
+  
+  DEFAULT_PROFILE: '',
+  
   TAG_INPUT_BTN_NO_TEXT : 'Enter tag name above',
   TAG_INPUT_BTN_WITH_TEXT : 'Create new tag',
   TAG_INPUT_BTN_EXIST_TAG : 'Tag already added'

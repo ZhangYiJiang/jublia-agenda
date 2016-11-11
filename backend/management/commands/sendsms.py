e25b74654a429982a103b1e4291a219ad3376ca8
Zhang Yi Jiang <mediumdeviation@gmail.com>
2016-11-11 16:36:59 +0800

    make sendsms command manual

--- backend/management/commands/sendsms.py
+++ backend/management/commands/sendsms.py
@@ -1,15 +1,10 @@
-from datetime import timedelta
-
 from django.conf import settings
 from django.core.management import BaseCommand
-from django.db import models
-from django.db.models import F, Func
-from django.utils import timezone
+from django.db.models import Func
 from django.utils.translation import ugettext as _
 from twilio.rest import Client
 
 from backend.models import Session
-
 
 # Check for sessions 15-20 minutes before their starting time
 NOTIFY_START = 15
@@ -40,36 +35,36 @@ class Timestamp(Func):
 
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('session_id', type=int)
+
     def handle(self, *args, **options):
-        notify_start = timezone.now() + timedelta(minutes=NOTIFY_START)
-        notify_end = timezone.now() + timedelta(minutes=NOTIFY_END)
-        start_time = Timestamp('agenda__start_at', output_field=models.IntegerField()) + (F('start_at') * 60)
-        sessions = Session.objects.annotate(start_time=start_time).filter(
-            is_sms_sent=False,
-            agenda__published=True,
-            start_time__gte=notify_start.timestamp(),
-            start_time__lte=notify_end.timestamp(),
-        ).prefetch_related('viewer_set').all()
+        session = Session.objects.filter(
+            pk=options['session_id'],
+            is_sms_sent=False
+        ).first()
+
+        if not session:
+            return
 
         client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
-        for session in sessions:
-            for viewer in session.viewer_set.exclude(mobile=''):
-                message = _('The session %(session)s you have bookmarked '
-                            'is starting soon.' % {
-                                'session': session.name,
-                            })
+        for viewer in session.viewer_set.exclude(mobile=''):
+            message = _('The session %(session)s you have bookmarked '
+                        'is starting soon.' % {
+                            'session': session.name,
+                        })
 
-                print('Sending SMS "%(message)s" to %(mobile)s' % {
-                    'mobile': viewer.mobile,
-                    'message': message,
-                })
+            print('Sending SMS "%(message)s" to %(mobile)s' % {
+                'mobile': viewer.mobile,
+                'message': message,
+            })
 
-                client.messages.create(
-                    to=viewer.mobile,
-                    messaging_service_sid=settings.TWILIO_COPILOT_SID,
-                    body=message,
-                )
+            client.messages.create(
+                to=viewer.mobile,
+                messaging_service_sid=settings.TWILIO_COPILOT_SID,
+                body=message,
+            )
 
-            session.is_sms_sent = True
-            session.save()
+        session.is_sms_sent = True
+        session.save()
